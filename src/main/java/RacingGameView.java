0df6bd1251df8e637cdbc2bc16954b180ed3883e
lenkim <joenggyu0@gmail.com>
2021-05-04 22:53:03 +0900

    fix: 시작하자마자 나오는 문자열 잘못 출력되는 버그 수정

--- src/main/java/RacingGameView.java
+++ src/main/java/RacingGameView.java
@@ -11,7 +11,7 @@ import racingcar.Winners;
 public class RacingGameView {
 
 	public void racingCarGameStart() {
-		System.out.println("경주할 자동차 이름을 입력하세.(이름은 쉼(,) 기준으 구분)");
+		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 	}
 
 	public void askingForRoundCountMessage() {
